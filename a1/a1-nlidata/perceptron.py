caf519136b06c713fcfeeed4ee006e39d083211a
Arif <arifyuali@gmail.com>
2016-10-08 13:19:14 -0400

    correct the for loop in score

--- a1/a1-nlidata/perceptron.py
+++ a1/a1-nlidata/perceptron.py
@@ -35,7 +35,7 @@ def load_featurized_docs(datasplit):
     assert len(rawdocs)==len(labels)>0,datasplit
     featdocs = []
     for d in rawdocs:
-        featdocs.append( vv(d))
+        featdocs.append( extract_feats(d))
     return featdocs, labels
 
 class Perceptron:
@@ -61,21 +61,16 @@ class Perceptron:
         parameters.
         """
         mistake = False
-        for inter in range(self.MAX_ITERATIONS):
-            #if mistake:
-            #    break
+        for iteration in range(self.MAX_ITERATIONS):
             for i in range(0, len(train_docs)):
                 label = train_labels[i]
-                weight_inter = {l: 0 for l in self.CLASSES}
-                #for l in self.CLASSES:
-                #    for word in train_docs[i]:
-                #        weight_inter[l] += self.weights[l][word]*train_docs[i][word]  
-                yhat = predict(train_docs[i])
-                if yhat != l:
+                yhat = self.predict(train_docs[i])
+                if yhat != label:
                     for word in train_docs[i]:
-                        self.weights[l][word] = self.weights[l][word] + train_docs[i][word]
+                        self.weights[label][word] = self.weights[label][word] + train_docs[i][word]
                         self.weights[yhat][word] = self.weights[yhat][word] - train_docs[i][word]
-                        
+            print("iteration: "+ str(iteration) +", train accuracy: "+ str(self.test_eval(train_docs, train_labels)) +", dev accuracy: " + str(self.test_eval(dev_docs, dev_labels)), file=sys.stderr)
+
 
     def score(self, doc, label):
         """
@@ -84,7 +79,7 @@ class Perceptron:
         """
         score = 0
         for word in doc:
-            score += self.weights[l][word]*doc[word]
+            score += self.weights[label][word]*doc[word]
         return score
 
     def predict(self, doc):
@@ -93,7 +88,7 @@ class Perceptron:
         """
         scores = {l: 0 for l in self.CLASSES}
         for l in self.CLASSES:
-            scores[l] += score(doc, l)
+            scores[l] += self.score(doc, l)
         label_pred = max(scores, key=scores.get)
         return label_pred
 
@@ -121,5 +116,4 @@ if __name__ == "__main__":
         sum(len(d) for d in test_docs)/len(test_docs), 'percepts on avg', file=sys.stderr)
 
     ptron = Perceptron(train_docs, train_labels, MAX_ITERATIONS=niters, dev_docs=dev_docs, dev_labels=dev_labels)
-    acc = ptron.test_eval(test_docs, test_labels)
-    print(acc, file=sys.stderr)
+ 
