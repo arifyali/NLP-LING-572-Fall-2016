215f6777e766bee3c3b7f626f0acee8ab7fcf31b
Arif <arifyuali@gmail.com>
2016-10-08 20:09:07 -0400

    removed dict method for predict because of issues with multiple max's

--- a1/a1-nlidata/perceptron.py
+++ a1/a1-nlidata/perceptron.py
@@ -60,15 +60,17 @@ class Perceptron:
         At the end of training, self.weights should contain the final model
         parameters.
         """
+        learning_tune = 0.5
         for iteration in range(self.MAX_ITERATIONS):
+            print("iteration: "+ str(iteration) +", train accuracy: "+ str(self.test_eval(train_docs, train_labels)) +", dev accuracy: " + str(self.test_eval(dev_docs, dev_labels)), file=sys.stderr)
+
             for i in range(0, len(train_docs)):
                 label = train_labels[i]
                 yhat = self.predict(train_docs[i])
                 if yhat != label:
                     for word in train_docs[i]:
-                        self.weights[label][word] += train_docs[i][word]
-                        self.weights[yhat][word] -= train_docs[i][word]
-            print("iteration: "+ str(iteration) +", train accuracy: "+ str(self.test_eval(train_docs, train_labels)) +", dev accuracy: " + str(self.test_eval(dev_docs, dev_labels)), file=sys.stderr)
+                        self.weights[label][word] += learning_tune*train_docs[i][word]
+                        self.weights[yhat][word] -= learning_tune*train_docs[i][word]
 
 
     def score(self, doc, label):
@@ -85,11 +87,16 @@ class Perceptron:
         """
         Return the highest-scoring label for the document under the current model.
         """
-        scores = {l: 0 for l in self.CLASSES}
-        for l in self.CLASSES:
-            scores[l] += self.score(doc, l)
-        label_pred = max(scores, key=scores.get)
-        return label_pred
+        max_label = self.CLASSES[0]
+        max_score = self.score(doc, max_label)
+        # note: the dict method from nbmodels don't work because there is a greater certainity of 
+        for l in self.CLASSES[1:]:
+            current_score = self.score(doc, l)
+            if current_score > max_score:
+                max_score = current_score
+                max_label = l
+        return max_label
+
 
     def test_eval(self, test_docs, test_labels):
         pred_labels = [self.predict(d) for d in test_docs]
